49042c0789adf46ef48ce016ec443c393bd0a20d
bgrana <bgrana@conwet.com>
2015-02-13 15:49:54 +0100

    Use Nova.getimagelist instead of Nova.getimagedetail for retrieving the details of a given image

--- src/js/imageDetails.js
+++ src/js/imageDetails.js
@@ -18,7 +18,27 @@ var ImageDetails = (function (JSTACK) {
 
 	ImageDetails.prototype = {
 		getImageDetails: function getImageDetails (callback, onError) {
-			JSTACK.Nova.getimagedetail(this.imageId, callback, onError);
+
+			var onOk = function onOk (response) {
+
+				var imageData;
+				
+				for (var i=0; i<response.images.length; i++) {
+					if (response.images[i].id === this.imageId) {
+						imageData = JSON.stringify(response.images[i]);
+						break;
+					}
+				}
+
+				if (!imageData) {
+					onError("Image with ID " + this.imageId + " does not exist.");
+					return;
+				}
+
+				callback(imageData);
+			}.bind(this);
+
+			JSTACK.Nova.getimagelist(true, onOk, onError);
 		},
 
 		deleteImage: function deleteImage (callback, onError) {
