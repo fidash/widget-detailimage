d6a5887eb4062d03016fc86f3155bbe5df364b5f
bgrana <bgrana@conwet.com>
2015-02-13 16:49:00 +0100

    Delete button is now disabled when the image is protected and visibility property is now correctly displayed

--- src/js/ui.js
+++ src/js/ui.js
@@ -20,18 +20,11 @@ var UI = (function () {
 
 	function UI () {
 
-		// Buttons and layout initialization
 		borderLayout = new StyledElements.BorderLayout();
-		refreshButton = new StyledElements.StyledButton({text:'Refresh', 'class': 'pull-right clear'});
-		deleteButton = new StyledElements.StyledButton({text:'Delete', 'class': 'btn-danger pull-right disabled'});
-
-
-		// Set click events
-		refreshButton.addEventListener('click', refresh.bind(this), false);
-		deleteButton.addEventListener('click', this.deleteImage.bind(this), false);
 
 		// Register callback for input endpoint
 		MashupPlatform.wiring.registerCallback('image_details', receiveImageId.bind(this));
+
 
 		/* Context */
 		MashupPlatform.widget.context.registerCallback(function (newValues) {
@@ -90,11 +83,13 @@ var UI = (function () {
 				statusList 	= document.createElement('ul'),
 				specsList 	= document.createElement('ul');
 
+			var visibility = imageData.is_public ? 'Public' : 'Private';
+
 			infoList.innerHTML = '<li><strong>' + fields[0] + ':</strong> ' + imageData.id + '</li>' +
 								 '<li><strong>' + fields[1] + ':</strong> ' + imageData.name + '</li>';
 
 			statusList.innerHTML = '<li><strong>' + fields[2] + ':</strong> ' + imageData.status + '</li>' +
-								   '<li><strong>' + fields[3] + ':</strong> ' + imageData.visibility + '</li>' +
+								   '<li><strong>' + fields[3] + ':</strong> ' + visibility + '</li>' +
 								   '<li><strong>' + fields[4] + ':</strong> ' + imageData.checksum + '</li>' +
 								   '<li><strong>' + fields[5] + ':</strong> ' + imageData.created_at + '</li>' +
 								   '<li><strong>' + fields[6] + ':</strong> ' + imageData.updated_at + '</li>';
@@ -102,6 +97,17 @@ var UI = (function () {
 			specsList.innerHTML = '<li><strong>' + fields[7] + ':</strong> ' + imageData.size + '</li>' +
 								  '<li><strong>' + fields[8] + ':</strong> ' + imageData.container_format + '</li>' +
 								  '<li><strong>' + fields[9] + ':</strong> ' + imageData.disk_format + '</li>';
+
+
+			// Buttons
+			var deleteButtonClass = imageData.protected ? 'btn-danger pull-right disabled' : 'btn-danger pull-right';
+			
+			refreshButton = new StyledElements.StyledButton({text:'Refresh', 'class': 'pull-right clear'});
+			deleteButton = new StyledElements.StyledButton({text:'Delete', 'class': deleteButtonClass});
+			
+
+			refreshButton.addEventListener('click', refresh.bind(this), false);
+			deleteButton.addEventListener('click', this.deleteImage.bind(this), false);
 
 
 			// Header and footer
