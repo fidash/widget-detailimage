82bb1726da56c7dff2ad0f529bcb5abe9d426e66
bgrana <bgrana@conwet.com>
2015-05-14 12:18:38 +0200

    Remove nonexistent build/helpers/ path from karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -20,7 +20,6 @@
 
         // Helper files
         'src/test/helpers/*.js',
-        'build/helpers/*.js',
 
         // Fixtures
         { pattern: 'src/test/fixtures/html/defaultTemplate.html',
